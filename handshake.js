dd249c0624ac92058e2422d271c147bc776ee5b5
Ollie Parsley <ollie@ollieparsley.com>
2014-01-16 14:26:12 +0000

    Added custom morse code function

--- handshake.js
+++ handshake.js
@@ -139,3 +139,83 @@ function countdown()	//	A 3 second countdown timer, suitable for a racing game.
 	//                 3       2       1       Go!
 	navigator.vibrate([300,700,300,700,300,700,1000]);
 }
+
+function morse(text)	// Vibrate morse code based on the supplied text
+{
+	//	Dot, dash gap and space
+	var dot = 80;
+	var dash = 300;
+	var gap = 100;
+	var space = 300;
+	
+	//	The patters for each letter of the alphabet, numbers and some punctuation
+	var morseLetters = {
+		"A": [dot, dash],
+		"B": [dash, dot, dot, dot],
+		"C": [dash, dot, dash, dot],
+		"D": [dash, dot, dot],
+		"E": [dot],
+		"F": [dot, dot, dash, dot],
+		"G": [dash, dash, dot],
+		"H": [dot, dot, dot, dot],
+		"I": [dot, dot],
+		"J": [dot, dash, dash, dash],
+		"K": [dash, dot, dash],
+		"L": [dot, dash, dot, dot],
+		"M": [dash, dash],
+		"N": [dash, dot],
+		"O": [dash, dash, dash],
+		"P": [dot, dash, dash, dot],
+		"Q": [dash, dash, dot, dash],
+		"R": [dot, dash, dot],
+		"S": [dot, dot, dot],
+		"T": [dash],
+		"U": [dot, dot, dash],
+		"V": [dot, dot, dot, dash],
+		"W": [dot, dash, dash],
+		"X": [dash, dot, dot, dash],
+		"Y": [dash, dot, dash, dash],
+		"Z": [dash, dash, dot, dot],
+		"0": [dash, dash, dash, dash, dash],
+		"1": [dot, dash, dash, dash, dash],
+		"2": [dot, dot, dash, dash, dash],
+		"3": [dot, dot, dot, dash, dash],
+		"4": [dot, dot, dot, dot, dash],
+		"5": [dot, dot, dot, dot, dot],
+		"6": [dash, dot, dot, dot, dot],
+		"7": [dash, dash, dot, dot, dot],
+		"8": [dash, dash, dash, dot, dot],
+		"9": [dash, dash, dash, dash, dot],
+		".": [dot, dash, dot, dash, dot, dash], 
+		",": [dash, dash, dot, dot, dash, dash],
+		":": [dash, dash, dash, dot, dot, dot],
+		"?": [dot, dot, dash, dash, dot, dot],
+		"'": [dot, dash, dash, dash, dash, dot],
+		"-": [dash, dot, dot, dot, dot, dash], 
+		"/": [dash, dot, dot, dash, dot],
+		"(": [dash, dot, dash, dash, dot, dash],
+		")": [dash, dot, dash, dash, dot, dash],
+		"\"": [dot, dash, dot, dot, dash, dot]
+	};
+	
+	//Loop through each letter
+	var pattern = [];
+	for(var i=0; i< text.length; i++) {
+		var letterVibe = morseLetters[text.charAt(i).toString().toUpperCase()];
+		if (text.charAt(i) === ' ') {
+			pattern.push(0);
+			pattern.push(space);
+		} else if (letterVibe !== undefined) {
+			letterVibe.forEach(function(vibe) {
+				pattern.push(vibe);
+				pattern.push(gap);
+			});
+		}
+	}
+	
+	console.log(pattern);
+	
+	//Vibrate the pattern
+	navigator.vibrate(pattern);
+
+}
